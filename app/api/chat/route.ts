8cf82956ec5c5769cfecf4ac943a63110b7f08f6
erzan12 <do.earljan@gmail.com>
2026-06-19 10:50:04 +0800

    feat: added new custom data about me

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,41 +1,41 @@
-import { NextRequest, NextResponse } from "next/server";
-import profile from "@/app/data/profile.json";
-import { openrouter } from "@/app/lib/openrouter";
+// import { NextRequest, NextResponse } from "next/server";
+// import profile from "@/app/data/profile";
+// import { openrouter } from "@/app/lib/openrouter";
 
-export async function POST(req: NextRequest) {
-  const { message } = await req.json();
+// export async function POST(req: NextRequest) {
+//   const { message } = await req.json();
 
-  const systemPrompt = `
-You are the personal AI assistant for ${profile.name}.
+//   const systemPrompt = `
+// You are the personal AI assistant for ${profile.name}.
 
-Answer questions about ${profile.name}
-using ONLY the information provided below.
+// Answer questions about ${profile.name}
+// using ONLY the information provided below.
 
-PROFILE:
-${JSON.stringify(profile, null, 2)}
+// PROFILE:
+// ${JSON.stringify(profile, null, 2)}
 
-If information is unavailable,
-say:
-"I don't have information about that."
-`;
+// If information is unavailable,
+// say:
+// "I don't have information about that."
+// `;
 
-  const completion =
-    await openrouter.chat.completions.create({
-      model: "openai/gpt-4o-mini",
-      messages: [
-        {
-          role: "system",
-          content: systemPrompt
-        },
-        {
-          role: "user",
-          content: message
-        }
-      ]
-    });
+//   const completion =
+//     await openrouter.chat.completions.create({
+//       model: "openai/gpt-4o-mini",
+//       messages: [
+//         {
+//           role: "system",
+//           content: systemPrompt
+//         },
+//         {
+//           role: "user",
+//           content: message
+//         }
+//       ]
+//     });
 
-  return NextResponse.json({
-    answer:
-      completion.choices[0].message.content
-  });
-}
+//   return NextResponse.json({
+//     answer:
+//       completion.choices[0].message.content
+//   });
+// }
